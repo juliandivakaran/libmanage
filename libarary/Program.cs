fcab8acd10b27b8249a7e372ac2d3e5c46a9baee
juliandivagaran <jndivagaran@gmail.com>
2024-12-24 10:19:00 -0800

    cretaed all endpoint

--- libarary/Program.cs
+++ libarary/Program.cs
@@ -1,20 +1,27 @@
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Rewrite;
+using Microsoft.AspNetCore.Builder;
+using System.Text.Json;
+
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+var todos = new List<Todo>();
 
 app.UseRewriter(new RewriteOptions().AddRedirect("tasks/(.*)", "todos/$1"));
-app.Use(async (context, next) =>{
-    Console.WriteLine($"[{context.Request.Method} {context.Request.Path}{DateTime.UtcNow}] Started.");
-    await next(context);
-    Console.WriteLine($"[{context.Request.Method} {context.Request.Path}{DateTime.UtcNow}] Finished.");
-    
-
+app.Use(async (context, next) =>
+{
+    Console.WriteLine($"[{context.Request.Method} {context.Request.Path} {DateTime.UtcNow}] Started.");
+    await next();
+    Console.WriteLine($"[{context.Request.Method} {context.Request.Path} {DateTime.UtcNow}] Finished.");
 });
 
+
+
+
 // In-memory list of todos
-var todos = new List<Todo>();
+//var todos = new List<Todo>();
+
 
 app.MapGet("/todos", () => todos);
 
@@ -30,9 +37,29 @@ app.MapGet("/todos/{id}", (int id) =>
 // POST endpoint to create a new todo
 app.MapPost("/todos", (Todo task) =>
 {
+     if (todos.Any(t => t.BookId == task.BookId))
+    {
+        return Results.Conflict($"A task with BookId {task.BookId} already exists.");
+    }
     todos.Add(task);
     return Results.Created($"/todos/{task.BookId}", task); // Use Results helper for Created
 });
+//.AddEndpointFilter(async(context, next)=>{
+ //   var taskArgument = context.GetArgument<Todo>(0);
+//    var errors = new Dictionary<string, string[]>();
+//    if (taskArgument.DueDate < DateTime.UtcNow)
+  //  {
+    //    errors.Add(nameof(Todo.Duedate), ["Cannot have due date in the past"]);
+//    }
+//   if (taskArgument.IsCompleted){
+ //       errors.Add(nameof(Todo.IsCompleted), ["Cannot add completed"]);
+//    }
+//    if (errors.Count > 0)
+//    {
+//        return Results.ValidationProblem(errors);
+//    }
+//    return await next(context);
+//}); 
 
 
 app.MapDelete("/todos/{id}", (int id) =>
@@ -44,5 +71,5 @@ app.MapDelete("/todos/{id}", (int id) =>
 app.Run();
 
 // Define the Todo record
-public record Todo(int BookId, string Author, string description, bool IsCompleted);
+public record Todo(int BookId, string Author, string Description);
 
--- libarary/obj/Debug/net9.0/libarary.AssemblyInfo.cs
+++ libarary/obj/Debug/net9.0/libarary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("libarary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca950923b184047205651648d0a1603401e4a25e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+504320fae75b8e18ab9dfd2d9d7a896077c919d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("libarary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("libarary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
